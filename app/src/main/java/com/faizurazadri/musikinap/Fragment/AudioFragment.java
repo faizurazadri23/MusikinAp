f7efe3c35a61e4605446bfd6eae4a464c0ce5615
faizurazadri23 <faizurazadri08@gmail.com>
2021-09-16 09:13:50 +0700

    audio

--- app/src/main/java/com/faizurazadri/musikinap/Fragment/AudioFragment.java
+++ app/src/main/java/com/faizurazadri/musikinap/Fragment/AudioFragment.java
@@ -1,7 +1,10 @@
 package com.faizurazadri.musikinap.Fragment;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -15,11 +18,27 @@ import com.faizurazadri.musikinap.databinding.FragmentAudioBinding;
 public class AudioFragment extends Fragment {
 
     private FragmentAudioBinding audioBinding;
+    private MediaPlayer mediaPlayer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         audioBinding = FragmentAudioBinding.inflate(inflater, container, false);
         return audioBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    private void state_awal(){
+
+    }
+
+    private void pause_audio1(){
+
     }
 }
